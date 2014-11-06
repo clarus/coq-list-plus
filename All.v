81189409cf43761d92a572af215d0c95da185123
Guillaume Claret <dev@clarus.me>
2014-11-06 16:26:15 +0100

    repeat function

--- All.v
+++ All.v
@@ -1,3 +1,15 @@
-Module List.
+Require Import Lists.List.
 
+Import ListNotations.
+
+Module List.
+  Fixpoint repeat_aux {A : Type} (x : A) (n : nat) (l : list A) : list A :=
+    match n with
+    | O => l
+    | S n => repeat_aux x n (x :: l)
+    end.
+
+  (** Make a list of `n` elements `x`. *)
+  Definition repeat {A : Type} (x : A) (n : nat) : list A :=
+    repeat_aux x n [].
 End List.
