f11bbe45e93ec4557a5797335f206a838da9c20b
Guillaume Claret <dev@clarus.me>
2015-02-23 14:53:51 +0100

    Function map_filter added

--- List.v
+++ List.v
@@ -19,3 +19,14 @@ Fixpoint remove_nones {A : Type} (l : list (option A)) : list A :=
   | None :: l => remove_nones l
   | Some x :: l => x :: remove_nones l
   end.
+
+(** Do a map removing the `None` results of `f`. *)
+Fixpoint map_filter {A B : Type} (f : A -> option B) (l : list A) : list B :=
+  match l with
+  | [] => []
+  | x :: l =>
+    match f x with
+    | None => map_filter f l
+    | Some y => y :: map_filter f l
+    end
+  end.
